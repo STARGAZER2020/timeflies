0e07829a69507e4f5b02aa99a0d2f391deec3619
Prashant Chaturvedi <prashantchaturvedi2020@gmail.com>
2017-12-30 10:37:42 +0530

    Update and rename graph to implementation.cpp

---
+++ implementation.cpp
@@ -0,0 +1,40 @@
+
+/* print all value level wise */
+#include<bits/stdc++.h>
+using namespace std; 
+#define ll long long
+vector<ll> g[100000];
+int v[100000]={0};
+void bfs(int s)
+{
+    v[s]=1;
+    queue<int> q;
+    q.push(s);
+    cout<<s<<" "; // print source node
+    while(!q.empty())
+    {
+        int w=q.front();
+        q.pop();
+        for(int i=0;i<g[w].size();++i)
+        {
+            if(v[g[w][i]]==0)
+            {   q.push(g[w][i]);
+                v[g[w][i]]=1;
+                cout<<g[w][i]<<" "; // print all node except source node
+             }
+        }
+    }
+}
+int main()
+{
+    int n,m,x,y;
+    cin>>n>>m;
+    for(int i=0;i<m;++i)
+    {
+        cin>>x>>y;
+        g[y].push_back(x);
+        g[x].push_back(y);
+    }
+    bfs(1);
+    return 0;
+}
